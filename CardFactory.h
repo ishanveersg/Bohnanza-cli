926b3e7474737f9144b39addd9ee6905f99a6b73
MichaelMIJ243 <91161686+MichaelMIJ243@users.noreply.github.com>
2021-12-08 11:46:58 -0500

    Add files via upload

---
+++ CardFactory.h
@@ -0,0 +1,30 @@
+#ifndef CardFactory_h
+#define CardFactory_h
+#include <random>
+#include <algorithm>
+#include <iterator>
+#include "Deck.h"
+#include "Card.h"
+#include "Blue.cpp"
+#include "Chili.cpp"
+#include "Stink.cpp"
+#include "Green.cpp"
+#include "Soy.cpp"
+#include "Black.cpp"
+#include "Red.cpp"
+#include "Garden.cpp"
+
+class Deck;
+
+class CardFactory {
+	Deck *deck;
+	CardFactory(const CardFactory&);
+	CardFactory();
+
+public:
+	static CardFactory* getFactory();
+	Deck getDeck();
+	Card* getCard(char);
+	Deck setDeck (istream &);
+};
+#endif
