64bb19b543a277a56571885410adedeb58eeb6ad
MichaelMIJ243 <91161686+MichaelMIJ243@users.noreply.github.com>
2021-12-08 12:02:54 -0500

    Update Hand.h

--- Hand.h
+++ Hand.h
@@ -1,16 +1,14 @@
-#pragma once
 #include "CardFactory.h"
+#include <vector>
 
 class Hand : public vector<Card*> {
-private:
-
-
 public:
-	Hand() = default;						//default constructor
-	Hand& operator+=(Card*);				//a joute une carte derricre la dernicre carte de la main.
-	Card* play();							//renvoie et supprime la premicre carte de la main du joueur .
-	Card* top();							//renvoie mais ne supprime pas la premicre carte de la main du joueur.
-	Card* operator[](int);					//renvoie et supprime une carte r une position donnée.
-	Hand(istream&, CardFactory*);		//constructeur qui accepte fichier
-	friend ostream & operator << (ostream &, Hand);
+	Hand() = default; // constructeur par defaut
+	Hand& operator+=(Card*);
+	//renvoie et supprime la premicre carte de la main du joueur .
+	Card* play();							
+	Card* top();	
+	Card* operator[](int);
+	Hand(istream&, CardFactory*);		//constructeur 1
+	friend ostream & operator << (ostream &, Hand);		// constructeur 2
 };
