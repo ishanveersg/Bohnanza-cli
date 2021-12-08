d764587a53f75a36fa5d0fca417d2236b397a59b
MichaelMIJ243 <91161686+MichaelMIJ243@users.noreply.github.com>
2021-12-08 11:19:46 -0500

    Update Hand.cpp

--- Hand.cpp
+++ Hand.cpp
@@ -2,38 +2,41 @@
 #include <string>
 #include <iostream>
 
-//back est la premiere carte 
-//size - position pour avoir la position
 
+//ajoute une carte a la fin de hand
 Hand& Hand ::operator+=(Card*cardToAdd) {  
 	(*this).insert(begin(), cardToAdd);
 	return *this;
 
-}//a joute une carte derrière la dernière carte de la main.
+}
 
+//renvoie la premier carte de hand
+//Supprime la carte supprimE
 Card* Hand :: play() {
 
 	Card* temp = (*this).back();
 	(*this).pop_back();
 	return temp;
 
-}//renvoie et supprime la première carte de la main du joueur .
+}
 
+//renvoie la premiere carte de hand
 Card* Hand ::top() {
 	return (*this).back();
-}//renvoie mais ne supprime pas la première carte de la main du joueur.
+}
 
+//Supprime une carte a 'position'
 Card* Hand ::operator[](int position) {
 	Card* temp = (*this).at((*this).size() - position);
 	(*this).erase( (*this).begin() + ((*this).size()-position) );
 	return temp;
-}//renvoie et supprime une carte à une position donnée.
+}
 
 
-Hand:: Hand(std::istream& in, CardFactory* cf) {
+Hand:: Hand(std::istream& x1, CardFactory* cf) {
 	char cardType[256];
-	in.getline(cardType, 256); ///dummy
-	in.getline(cardType, 256);
+	x1.getline(cardType, 256);
+	x1.getline(cardType, 256);
 
 	int cnt = 0;
 	while (cardType[cnt] != NULL) {
@@ -45,17 +48,12 @@ Hand:: Hand(std::istream& in, CardFactory* cf) {
 		(*this).push_back(cardToAdd);
 	}
 
-}													
+}	
 
-
- /*
- Text File Format:
- Line 1: Char for each card
- */
-ostream & operator<<(ostream & out, Hand h)
-{
+ostream & operator<<(ostream & out, Hand _hand){
+	
 	// TODO: make sure cards are in order
-	for (vector<Card*>::reverse_iterator it = h.rbegin(); it != h.rend(); it++) {
+	for (vector<Card*>::reverse_iterator it = _hand.rbegin(); it != _hand.rend(); it++) {
 		out << (*it);
 	}
 	return out;
